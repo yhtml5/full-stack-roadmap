52ad28d15711d0e676036456f9da59bac99247f2
张大漾丨前端开发 <1417376255@qq.com>
2016-03-09 22:43:14 +0800

    study fis3

---
+++ nodejs/demo-fis3/fis3-base-master/README.md
@@ -0,0 +1,17 @@
+# [fis3 base](https://github.com/yanhaijing/fis3-base) [![](https://img.shields.io/badge/Powered by-fis3 base-brightgreen.svg)](https://github.com/yanhaijing/fis3-base) [![license](http://img.shields.io/npm/l/express.svg)](https://github.com/yanhaijing/fis3-base/blob/master/MIT-LICENSE)
+基于fis3的纯前端解决方案，拿来即用的fis3脚手架。
+
+## 特色
+fis3 base有如下功能特色。
+- commonjs
+- ECMASCript2015
+- SASS&Compass
+- fis components
+- [data.js](https://github.com/yanhaijing/data.js)
+- [template.js](https://github.com/yanhaijing/template.js)
+- html，js，css组件
+- 本地模拟数据
+- 开发，测试发布，发布多种模式
+
+## 说明文档
+[INTRO](INTRO.md)
--- nodejs/new/fis-conf.js
+++ nodejs/new/fis-conf.js
@@ -1,33 +1,71 @@
-// 加 md5
-fis.match('*.{js,css,png}', {
-  useHash: true
-});
-// 启用 fis-spriter-csssprites 插件
-fis.match('::package', {
-  spriter: fis.plugin('csssprites')
-})
-// 对 CSS 进行图片合并
-fis.match('*.css', {
-  // 给匹配到的文件分配属性 `useSprite`
-  useSprite: true
-});
-fis.match('*.js', {
-  // fis-optimizer-uglify-js 插件进行压缩，已内置
-  optimizer: fis.plugin('uglify-js')
-});
-fis.match('*.css', {
-  // fis-optimizer-clean-css 插件进行压缩，已内置
-  optimizer: fis.plugin('clean-css')
-});
-fis.match('*.png', {
-  // fis-optimizer-png-compressor 插件进行压缩，已内置
-  optimizer: fis.plugin('png-compressor'),
-  release: '/dist/res/$0'
-});
-fis.media('debug').match('*.{js,css,png}', {
-  useHash: false,
-  useSprite: false,
-  optimizer: null
-})
+// 设置项目属性
+fis.set('project.name', 'fis3-base');
+fis.set('project.md5Length', 8);
+fis.set('project.md5Connector ', '.');
+fis.set('project.files', ['map.json', '/test/*']);
+fis.set('project.ignore', ['output/**', 'node_modules/**', '.git/**', '.svn/**']);
 
+// 所有的文件产出到 static/ 目录下
+fis.match('*', {
+	release: '/static/$0'
+});
 
+// 所有模板放到 tempalte 目录下
+fis.match('*.html', {
+	release: '/template/$0'
+});
+
+// widget源码目录下的资源被标注为组件
+fis.match('/widget/**/*', {
+	isMod: true
+});
+
+// widget下的 js 调用 jswrapper 进行自动化组件化封装
+fis.match('/widget/**/*.js', {
+	postprocessor: fis.plugin('jswrapper', {
+		type: 'commonjs'
+	})
+});
+
+// test 目录下的原封不动产出到 test 目录下
+fis.match('/test/**/*', {
+	release: '$0'
+});
+
+// optimize
+fis.media('prod')
+	.match('*.js', {
+		optimizer: fis.plugin('uglify-js', {
+			mangle: {
+				expect: ['require', 'define', 'some string'] //不想被压的
+			}
+		})
+	})
+	.match('*.css', {
+		optimizer: fis.plugin('clean-css', {
+			'keepBreaks': true //保持一个规则一个换行
+		})
+	});
+
+// pack
+fis.media('prod')
+	// 启用打包插件，必须匹配 ::package
+	.match('::package', {
+		packager: fis.plugin('map'),
+		spriter: fis.plugin('csssprites', {
+			layout: 'matrix',
+			margin: '15'
+		})
+	})
+	.match('*.js', {
+		packTo: '/static/all_others.js'
+	})
+	.match('*.css', {
+		packTo: '/staitc/all_others.css'
+	})
+	.match('/widget/**/*.js', {
+		packTo: '/static/all_comp.js'
+	})
+	.match('/widget/**/*.css', {
+		packTo: '/static/all_comp.css'
+	});
