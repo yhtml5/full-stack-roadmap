38432d5ae7861cc122d8cc143d3e4448cc70b245
张大漾丨前端开发 <1417376255@qq.com>
2016-03-15 13:25:54 +0800

    1

--- 极客学院/jQuery/js/第13课 noConfilct.js
+++ 极客学院/jQuery/js/第13课 noConfilct.js
@@ -1,10 +1,7 @@
 //$.noConflict()替换美元符号为jQuery
-var yhtml5=$.noConflict()//替换美元符号为yhtml5
-yhtml5(document).ready(function () {
-    yhtml5("#btn").on("click",function () {
-        yhtml5("div").text("new world")
-      
-    })
-    
-  
+var yhtml5 = $.noConflict() //替换美元符号为yhtml5
+yhtml5(document).ready(function() {
+	yhtml5("#btn").on("click", function() {
+		yhtml5("div").text("new world")
+	})
 })
