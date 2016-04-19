401a27031eaea0119a5deff97879cba737e1c889
张大漾丨前端开发 <1417376255@qq.com>
2016-04-19 11:35:39 +0800

    11

--- AngularJS/00_demo/ng-admin-demo/js/segments/segments.js
+++
@@ -1,6 +0,0 @@
-var segments = ['compulsive', 'collector', 'ordered_once', 'regular', 'returns', 'reviewer']
-
-export default {
-	values: segments,
-	choices: segments.map(segment => { return { label: segment, value: segment } })
-};
