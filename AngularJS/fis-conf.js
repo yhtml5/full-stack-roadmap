ff292c62af2da23d8e91aec106525f7a54d470d5
张大漾丨前端开发 <1417376255@qq.com>
2016-04-28 21:49:52 +0800

    11

---
+++ AngularJS/fis-conf.js
@@ -0,0 +1,12 @@
+// 设置项目属性
+fis.set('project.name', 'yhtml5-fis3');
+fis.set('project.md5Length', 8);
+fis.set('project.md5Connector ', '_');
+fis.set("project.ignore", [''])
+fis.match('/**', {
+	release: '/tutorial/runoob',
+})
+fis.match('{/*.js,/*.css}', {
+	release: '/static/img/$1',
+	url: '../static/img/$1',
+})
