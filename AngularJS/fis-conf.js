83125f9bd04e733dd2143357a93cad8a5d6a5856
张大漾丨前端开发 <1417376255@qq.com>
2016-04-28 21:59:05 +0800

    11

--- AngularJS/fis-conf.js
+++ AngularJS/fis-conf.js
@@ -4,7 +4,7 @@ fis.set('project.md5Length', 8);
 fis.set('project.md5Connector ', '_');
 fis.set("project.ignore", [''])
 fis.match('/**', {
-	release: '/tutorial/runoob',
+	release: '/runoob',
 })
 fis.match('{/*.js,/*.css}', {
 	release: '/static/img/$1',
