36dd21b583a0a019515ab4a0cb279b07b43beaed
张大漾 <1417376255@qq.com>
2018-07-05 16:21:21 +0800

    update

--- web/javascript/eventLoop/index.js
+++ web/javascript/eventLoop/index.js
@@ -5,52 +5,53 @@ const timeoutA = setTimeout(function () {
   console.log('timeoutA', new Date() - timeA);
 }, 2000)
 
-const promiseA = new Promise(function (resolve) {
-  console.log('promiseA', new Date() - timeA);
-  resolve()
-  console.log('promiseA0', new Date() - timeA);
-})
-  .then(function () {
-    console.log('promiseA1', new Date() - timeA);
-  })
-  .then(function () {
-    console.log('promiseA2', new Date() - timeA);
-  })
+// const promiseA = new Promise(function (resolve) {
+//   console.log('promiseA', new Date() - timeA);
+//   resolve()
+//   console.log('promiseA0', new Date() - timeA);
+// })
+//   .then(function () {
+//     console.log('promiseA1', new Date() - timeA);
+//   })
+//   .then(function () {
+//     console.log('promiseA2', new Date() - timeA);
+//   })
 
-const timeB = new Date();
-while (new Date() - timeB < 1000); // 阻塞
+// console.log('while 1000');
+// const timeB = new Date();
+// while (new Date() - timeB < 1000); // 阻塞
 
-Promise.resolve()
-  .then(function () {
-    console.log('promiseB1', new Date() - timeA);
-  })
-  .then(function () {
-    console.log('promiseB2', new Date() - timeA);
-  })
-  .then(function () {
-    console.log('promiseB3', new Date() - timeA);
-  })
-
-for (var i = 0; i < 5; i++) {
-  (function (j) {
-    setTimeout(() => {
-      console.log('timeoutB' + i, new Date() - timeA);
-    }, 1000 * i);
-  })(i)
-}
-
-for (var j = 5; j < 9; j++) {
-  setTimeout(function (t) {
-    console.log('timeoutC' + t, new Date() - timeA);
-  }, 1000 * j, j);
-}
-
-promiseA
-  .then(() => console.log('promiseA3', new Date() - timeA))
-
-console.log('script end', new Date() - timeA);
+// Promise.resolve()
+//   .then(function () {
+//     console.log('promiseB1', new Date() - timeA);
+//   })
+//   .then(function () {
+//     console.log('promiseB2', new Date() - timeA);
+//   })
+//   .then(function () {
+//     console.log('promiseB3', new Date() - timeA);
+//   })
 
 
+// for (var i = 0; i < 5; i++) {
+//   setTimeout(function () {
+//     console.log(i)
+//   }, 1000 * i);
+// }
+
+// for (var i = 0; i < 5; i++) {
+//   (function (j) {
+//     setTimeout(() => {
+//       console.log('timeoutB' + i, new Date() - timeA);
+//     }, 1000 * i);
+//   })(i)
+// }
+
+// for (var j = 5; j < 9; j++) {
+//   setTimeout(function (t) {
+//     console.log('timeoutC' + t, new Date() - timeA);
+//   }, 1000 * j, j);
+// }
 
 // for (let i = 0; i < 5; i++) {
 //   setTimeout(() => {
@@ -58,10 +59,10 @@ console.log('script end', new Date() - timeA);
 //   }, 1000 * i);
 // }
 
+// promiseA
+//   .then(() => console.log('promiseA3', new Date() - timeA))
 
-
-
-
+console.log('script end', new Date() - timeA);
 
 
 
