ac4ec64b769df9d49846dbf3a049153d7bcb2f29
张大漾 <1417376255@qq.com>
2018-07-05 19:34:43 +0800

    update

--- web/javascript/eventLoop/index.js
+++ web/javascript/eventLoop/index.js
@@ -1,69 +1,51 @@
 const timeA = new Date();
-console.log('script start', new Date() - timeA);
-
+const timeNow = () => new Date() - timeA
+console.log(timeNow(), 'script start');
 const timeoutA = setTimeout(function () {
-  console.log('timeoutA', new Date() - timeA);
+  console.log(timeNow(), 'timeoutA');
 }, 2000)
+const promiseA = new Promise(function (resolve) {
+  console.log(timeNow(), 'promiseA');
+  resolve()
+  console.log(timeNow(), 'promiseA0');
+}).then(function () {
+  console.log(timeNow(), 'promiseA1');
+})
+console.log(timeNow(), 'while 1000');
+const timeB = new Date();
+while (new Date() - timeB < 1000);
+Promise.resolve()
+  .then(function () {
+    console.log(timeNow(), 'promiseB1');
+  })
+  .then(function () {
+    console.log(timeNow(), 'promiseB2');
+  })
+for (var i = 0; i < 5; i++) {
+  setTimeout(function () {
+    console.log(timeNow(), 'timeoutB', i);
+  }, 1000 * i);
+}
+for (let k = 0; k < 5; k++) {
+  setTimeout(() => {
+    console.log(timeNow(), 'timeoutC', k);
+  }, 1000 * k);
+}
+promiseA.then(() => console.log(timeNow(), 'promiseA3'))
+console.log(timeNow(), 'script end');
 
-// const promiseA = new Promise(function (resolve) {
-//   console.log('promiseA', new Date() - timeA);
-//   resolve()
-//   console.log('promiseA0', new Date() - timeA);
-// })
-//   .then(function () {
-//     console.log('promiseA1', new Date() - timeA);
-//   })
-//   .then(function () {
-//     console.log('promiseA2', new Date() - timeA);
-//   })
-
-// console.log('while 1000');
-// const timeB = new Date();
-// while (new Date() - timeB < 1000); // 阻塞
-
-// Promise.resolve()
-//   .then(function () {
-//     console.log('promiseB1', new Date() - timeA);
-//   })
-//   .then(function () {
-//     console.log('promiseB2', new Date() - timeA);
-//   })
-//   .then(function () {
-//     console.log('promiseB3', new Date() - timeA);
-//   })
-
-
-// for (var i = 0; i < 5; i++) {
-//   setTimeout(function () {
-//     console.log(i)
-//   }, 1000 * i);
+// for (var j = 5; j < 9; j++) {
+//   setTimeout(function (t) {
+//     console.log('timeoutC' + t, timeNow());
+//   }, 1000 * j, j);
 // }
 
 // for (var i = 0; i < 5; i++) {
 //   (function (j) {
 //     setTimeout(() => {
-//       console.log('timeoutB' + i, new Date() - timeA);
+//       console.log('timeoutB' + i, timeNow());
 //     }, 1000 * i);
 //   })(i)
 // }
-
-// for (var j = 5; j < 9; j++) {
-//   setTimeout(function (t) {
-//     console.log('timeoutC' + t, new Date() - timeA);
-//   }, 1000 * j, j);
-// }
-
-// for (let i = 0; i < 5; i++) {
-//   setTimeout(() => {
-//     console.log(i)
-//   }, 1000 * i);
-// }
-
-// promiseA
-//   .then(() => console.log('promiseA3', new Date() - timeA))
-
-console.log('script end', new Date() - timeA);
-
-
 
 
